ae6b7b028d4af5dfd260adbc1fc2d496ac931f39
kh4l1d <khalidahmedsayed@gmail.com>
2016-10-17 18:39:30 +0530

    Corrected the loops and added display function

--- travellingSalesman.cpp
+++ travellingSalesman.cpp
@@ -10,7 +10,7 @@
 #include<omp.h>
 using namespace std;
 
-int graph[5][5],cost = 999;
+int graph[5][5],cost = 9999;
 
 void swap (int *x, int *y)  // the sequence of traversing cities will be traversed
 {
@@ -25,7 +25,7 @@ void copy_array(int *a, int n)  // got the optimal sequence - now find the optim
     int i, sum = 0;
 
     #pragma omp parallel for
-      for(i = 0; i <= n; i++)   // notice the <=
+      for(i = 0; i < n; i++)   // the error was here - there was no need of <= - credits to Karan Chawda
       {
         sum += graph[a[i % 5]][a[(i + 1) % 5]]; // works similar to a counter
       }
@@ -57,6 +57,16 @@ void permute(int *a, int i, int n)
     }
 }
 
+
+void display_sequence(int *a,int n)
+{
+  int i;
+  cout<<"\nThe sequence of cities to be traversed is - \n";
+  for(i=0;i<n;i++)
+    cout<<"City "<<a[i]<<" ->\t";
+  cout<<"City "<<a[i]<<"\n";  // display all 5 cities
+}
+
 int main()
 {
     cout<<"Enter the elements for 5*5 array";
@@ -74,5 +84,8 @@ int main()
     int a[] = {0, 1, 2, 3,4}; // these are the cities to be traversed
 
    permute(a, 0, 4);
-   cout<<"\n\n\t\tminimum cost:"<<cost<<endl;
+   cout<<"\n\n\t\tMinimum cost = "<<cost<<endl;
+   display_sequence(a,4);
+
+   return 0;
 }
