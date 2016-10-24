91a8c05fccbcaba3aff7489973eb308df09b93c1
kh4l1d <khalidahmedsayed@gmail.com>
2016-10-24 14:51:20 +0530

    Custom input

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,55 +1,47 @@
+// Author: Khalid - naam toh suna hi hoga
+// Steps to run ->
+// :~$ g++ yoyo.cpp
+// :~$ ./a.out
+
 #include<iostream>
-#define N 10
 using namespace std;
-int binarySearch(int input_list[], int L, int R, int search_num ){
-  int index = 0;
 
-  // The element was not found in the list
-  if (L > R) {
-    return -1;
-  }
+int main()
+{
+    int n,i,key;
 
-  int mid = (L + R) / 2;
+    cout<<"\nEnter the length of array :";
+    cin>>n;
 
-  if (search_num > input_list[mid]) {
-    // Search in the upper half
-    index = binarySearch(input_list, mid + 1, R, search_num);
-  }
-  else if (search_num < input_list[mid]) {
-    // Search in the lower half
-    index = binarySearch(input_list, L, mid - 1, search_num);
-  }
-  else {
-    // Element found
-    index = mid;
-  }
-  return index;
-}
+    int a[n];
 
-int main() {
-  int num_list[N] = { 3, 4, 6, 8, 9, 11, 34, 47, 99, 100 };
-  // TODO add user input
-  int target_num = num_list[0];
-  int target_num_index = 0;
+    cout<<"\nPlease enter all "<<n<<" numbers in ascending order :";
+    for(i=0;i<n;i++)
+        cin>>a[i];
 
-  int start_index = 0, end_index = N - 1;
+    cout<<"\nPlease enter the number to be searched : ";
+    cin>>key;
 
-  for (size_t i = 0; i < 10; i++) {
-    cout << num_list[i] << " ";
-  }
+    int left=0,right=(n-1);
+    int mid=((left+right)/2);
 
-  cout << " " << endl;
+    while(left <= right)
+    {
+        if(a[mid] == key)
+        {
+            cout<<"\n"<<key<<" was found at position "<<(mid+1)<<" !!";
+            break;
+        }
+        else if(a[mid] < key)
+            left = mid+1;
+        else
+            right = mid-1;
 
-  cout << "Which number do you want to search?" << endl;
-  cin >> target_num;
+        mid = ((left+right)/2);   // remember to modify the mid
+    }
 
-  target_num_index = binarySearch(num_list, start_index, end_index, target_num);
+    if(left > right)
+        cout<<"\nThe number "<<key<<" is not in the array of numbers you entered.";
 
-  if (target_num_index >= 0) {
-    cout << "The number was found at index: " << target_num_index << endl;
-  }
-  else{
-    cout << "The number was not found." << endl;
-  }
-  return 0;
+    return 0;
 }
